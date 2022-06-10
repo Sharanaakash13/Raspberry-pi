f0eb914b84c9a3507722ebfddf9eb9918e087d2f
Sharanaakash13 <sharanaakash1310@gmail.com>
2022-06-10 20:26:22 +0000

    updating the code

--- ba_server_light/ferry_light.py
+++ ba_server_light/ferry_light.py
@@ -35,13 +35,13 @@ class Subscriber(Node):
         # Node for the Ferry light
         super().__init__('light_subscriber')
 
-        ''' # Checking the serial port 
+        # Checking the serial port 
         if not os.path.exists(self.Serial_port):
             self.get_logger().error("Serial port does not exists :" + self.Serial_port + "command not sent to the relay board")
-            rclpy.shutdown()'''
+            rclpy.shutdown()
 
         # Start the serial communication
-        '''self.ser = serial.Serial(
+        self.ser = serial.Serial(
             port=self.Serial_port, 
             baudrate=19200, 
             parity= serial.PARITY_NONE, 
@@ -51,7 +51,7 @@ class Subscriber(Node):
         
         # Checking the serial port is open 
         if(ser.isOpen()==False):
-            ser.open() '''     # Opens the serial port 
+            ser.open()      # Opens the serial port 
 
         # Initializing relay board 
         #command = 
@@ -60,12 +60,17 @@ class Subscriber(Node):
         # Creating Boolean subscription 
         self.subscription = self.create_subscription(Bool,'/topic', self.send_serial_cmd, 10)
         self.get_logger().info("Serial communication started")
+        self.subscription # prevent unused variable warning
 
         # Creating String publisher (Publishing the status)
         self.publisher = self.create_publisher(String,'/topic',10)
 
+        # Timer callback for publisher
+        self.time_period = 10  # seconds 
+        self.timer = self.create_timer(self.time_period, self.read_serial)
+
     def send_serial_cmd(self,msg):
-        
+
         # Relay board toogles to Docking light if the message is true.
         if msg.data == True:
             self.get_logger().info("Initiating docking lights")
@@ -78,7 +83,7 @@ class Subscriber(Node):
             self.get_logger().info("Sending serial : 8-1-00000001-XOR")
             self.send_serial(self.bytearray_1)
             
-            # Response from the relay board
+            ''' # Response from the relay board
             if(self.ser.readline() ==5):
                 self.get_logger().error("Error : Something went wrong in the relay board!!")
                 status = "Error in the relay board"
@@ -91,37 +96,59 @@ class Subscriber(Node):
                 response[0] = b'11110111'         # command response for toggle is 247 
                 status = "Successfully turned docking lights on."
                 self.publisher.publish(status)    # Publishing status to the same topic 
-                self.get_logger().info("Docking lights turned on")
+                self.get_logger().info("Docking lights turned on") '''
 
         else :
-            # serial code 
             self.get_logger().info("Initiating Navigation lights")
+            
             # serial code
             # Creating a byte array for docking light 
-            self.toggle2 = [cmd_toggle, address_relay, Nav_light,Nav_XOR]
-            self.bytearray_2 = bytearray(self.toggle2)
+            '''self.toggle2 = [cmd_toggle, address_relay, Nav_light,Nav_XOR]
+            self.bytearray_2 = bytearray(self.toggle2)'''
 
             # Sending the signal
             self.get_logger().info("Sending serial : 8-1-00000010-XOR")
             self.send_serial(self.bytearray_2)
-            
-            # Response from the relay board
-            if(self.ser.readline() ==5):
-                self.get_logger().error("Error : Something went wrong in the relay board!!")
-                status = "Error in the relay board"
-                self.publisher.publish(status)    # Publishing status to the same topic 
-                rclpy.shutdown()
-
-            else : 
-                response = self.ser.readline()    # response in bytes 
-                print("Response : ",response)
-                response[0] = b'11110111'         # command response for toggle is 247 
-                status = "Successfully turned navigation lights on."
-                self.publisher.publish(status)    # Publishing status to the same topic 
-                self.get_logger().info("Navigation lights turned on")
-    
+   
     def send_serial(self, send):
         self.ser.write(send)
+
+    def read_serial(self):
+
+        # Read serial from the relay board
+        self.response_serial = self.ser.read(1)     ###### .decode()
+
+        print('Response : ' + len(self.response_serial))
+        
+        # Response command  
+        ####self.response_command = bytes(247)  # when toggle is succesfull
+        self.response_error = bytes(255)    # When it is an error 
+
+        while True : #if self.serial_response == error_response:
+            self.var = False 
+            self.pub_status()       # publishing the status 
+            #rclpy.shutdown()
+
+        else : 
+            self.var = True 
+            self.pub_status()       # publishing the status '''
+
+
+    def pub_status(self):
+        # Defining message type for publisher 
+        status = String()
+
+        # Condition for publisher 
+        if self.var == False:           # if serial has error 
+            status.data = "Something went wrong....."
+            self.get_logger().info("Error : Something went wrong!!")
+       
+        else :                          # if serial has working properly     
+            status.data = "Successfully lights turned on."
+            self.get_logger().info("Relay board : Lights turned on")
+        
+        self.publisher.publish(status)    # Publishing status to the same topic 
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -129,8 +156,7 @@ def main(args=None):
     rclpy.spin(topic_subscriber)
     rclpy.shutdown()
 
-    print('Hi from ba_server_light.')
-
-
 if __name__ == '__main__':
     main()
+
+
